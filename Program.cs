a4e13f401081006c977a5530b842b10c9aa050a2
FelipeSoouza <146121300+FelipeSoouza@users.noreply.github.com>
2024-04-15 18:00:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            switch (nome)
+            {
+                case "João":
+                    Console.WriteLine("Olá João!");
+                    break;
+
+                case "Marcio":
+                    Console.WriteLine("Olá Marcio!");
+                    break;
+
+                case "Denis":
+                    Console.WriteLine("Olá Denis!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Olá {nome}!");
+                    break;
+
+            }
+
+        }
+    }
+}
